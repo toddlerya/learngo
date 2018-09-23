adb1abc7f7466baf35bc7431713c93aa4dcdaaeb
toddlerya <5418150+toddlerya@users.noreply.github.com>
2018-09-23 10:25:57 +0800

    postorder

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -1,6 +1,25 @@
 package main
 
-import "github.com/toddlerya/learngo/tree"
+import (
+	"fmt"
+
+	"github.com/toddlerya/learngo/tree"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
 
 func main() {
 	var root tree.Node
@@ -12,4 +31,8 @@ func main() {
 	root.Right.Left.SetValue(4)
 
 	root.Traverse()
+	fmt.Println()
+	myRoot := myTreeNode{&root}
+	myRoot.postOrder()
+	fmt.Println()
 }
