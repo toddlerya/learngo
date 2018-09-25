eee62cae1de351c6b23ce7d7fb35fbd34bf2815b
toddlerya <toddlerya@gmail.com>
2018-09-25 20:31:44 +0800

    add maps

---
+++ container/maze/maps.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string]string{
+		"name":    "ccmouse",
+		"course":  "golang",
+		"site":    "imooc",
+		"quality": "notbad",
+	}
+
+	m2 := make(map[string]int)
+	fmt.Println(m, m2)
+}
