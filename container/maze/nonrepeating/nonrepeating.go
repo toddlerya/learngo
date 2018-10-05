160633b788056e18beb573da43ad5b7aea4b5213
toddlerya <toddlerya@gmail.com>
2018-10-05 22:16:24 +0800

    benchmark

--- container/maze/nonrepeating/nonrepeating.go
+++ container/maze/nonrepeating/nonrepeating.go
@@ -4,10 +4,35 @@ import (
 	"fmt"
 )
 
+// var lastOccurred = make([]int, 0xffff)
+
+// func lengthOfNonRepeatingSubStr(s string) int {
+// 	// stores last occurred pos + 1.
+// 	// 0 means not seen.
+
+// 	for i := range lastOccurred {
+// 		lastOccurred[i] = 0
+// 	}
+
+// 	start := 0
+// 	maxLength := 0
+// 	for i, ch := range []rune(s) {
+// 		if lastI := lastOccurred[ch]; lastI > start {
+// 			start = lastI
+// 		}
+// 		if i-start+1 > maxLength {
+// 			maxLength = i - start + 1
+// 		}
+// 		lastOccurred[ch] = i + 1
+// 	}
+// 	return maxLength
+// }
+
 func lengthOfNonRepeatingSubStr(s string) int {
 	lastOccurred := make(map[rune]int)
 	start := 0
 	maxLength := 0
+
 	for i, ch := range []rune(s) {
 		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
 			start = lastI + 1
---
+++ container/maze/nonrepeating/nonrepeating_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSubStr(t *testing.T) {
+	tests := []struct {
+		s   string
+		ans int
+	}{
+		// Normal cases
+		{"abcabcbb", 3},
+		{"pwwkew", 3},
+
+		// Edge cases
+		{"", 0},
+		{"b", 1},
+		{"bbbb", 1},
+		{"abcabcabcd", 4},
+
+		// Chinese Support
+		{"这里是慕课网", 6},
+		{"一二三二一", 3},
+		{"黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花", 8},
+	}
+
+	for _, tt := range tests {
+		acutal := lengthOfNonRepeatingSubStr(tt.s)
+		if acutal != tt.ans {
+			t.Errorf("go %d for input %s; expected %d", acutal, tt.s, tt.ans)
+		}
+	}
+}
+
+func BenchmarkSubStr(b *testing.B) {
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s = s + s
+	}
+	b.Logf("len(s) = %d", len(s))
+	ans := 8
+
+	b.ResetTimer() 
+	for i := 0; i < b.N; i++ {
+		acutal := lengthOfNonRepeatingSubStr(s)
+		if acutal != ans {
+			b.Errorf("go %d for input %s; expected %d", acutal, s, ans)
+		}
+	}
+}
