9a1bcd3771ef647166cb2ec3b55788146c517b2b
toddlerya <toddlerya@gmail.com>
2018-10-07 20:40:48 +0800

    channel

--- channel/channel.go
+++ channel/channel.go
@@ -5,21 +5,29 @@ import (
 	"time"
 )
 
-func CreateWorker(id int) chan<- int {
+func worker(id int, c chan int) {
+	// for {
+	// 	n, ok := <-c
+	// 	if !ok {
+	// 		break
+	// 	}
+	// 	fmt.Printf("worker %d received %d \n", id, n)
+	// }
+	for n := range c {
+		fmt.Printf("worker %d received %d \n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
 	c := make(chan int)
-	go func() {
-		for {
-			n := <-c
-			fmt.Printf("worker %d received %c \n", id, n)
-		}
-	}()
+	go worker(id, c)
 	return c
 }
 
 func chanDemo() {
 	var channels [10]chan<- int
 	for i := 0; i < 10; i++ {
-		channels[i] = CreateWorker(i)
+		channels[i] = createWorker(i)
 	}
 	for i := 0; i < 10; i++ {
 		channels[i] <- 'a' + i
@@ -32,13 +40,27 @@ func chanDemo() {
 
 func bufferedChannel() {
 	c := make(chan int, 3)
-	c <- 1
-	c <- 2
-	c <- 3
-	c <- 4
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	time.Sleep(time.Millisecond)
+}
+
+func channelClose() {
+	c := make(chan int)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	close(c)
+	time.Sleep(time.Millisecond)
 }
 
 func main() {
-	// chanDemo()
-	bufferedChannel()
+	chanDemo()
+	// bufferedChannel()
+	// channelClose()
 }
